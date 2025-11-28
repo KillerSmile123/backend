9f33298f19ceefac50117016b8b9a93bd42e778c
Mark Godwill <markgodwill15@gmail.com>
2025-11-28 10:26:41 +0800

    Fix import and update DB URL

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 db = SQLAlchemy()
 
 def init_db(app: Flask):
-    # Railway MySQL connection
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    name = os.getenv("DB_NAME")
+
     app.config['SQLALCHEMY_DATABASE_URI'] = (
-        'mysql+pymysql://root:cSpXneUycqnISaIiJmhUbYzcpPgjUVOR@interchange.proxy.rlwy.net:18561/railway'
+        f'mysql+pymysql://{user}:{password}@{host}:{port}/{name}?charset=utf8mb4'
     )
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
